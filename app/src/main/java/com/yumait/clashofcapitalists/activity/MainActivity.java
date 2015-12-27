ba922a0dda5471b964b8f808cf57dc777a9e1acf
Bilel Methnani <bmethnani@anypli.com>
2015-12-27 06:11:05 +0100

    adding pure2d

--- app/src/main/java/com/yumait/clashofcapitalists/activity/MainActivity.java
+++ app/src/main/java/com/yumait/clashofcapitalists/activity/MainActivity.java
@@ -69,7 +69,6 @@ public class MainActivity extends AppCompatActivity {
             }
         });
 
-        //((RelativeLayout)findViewById(R.id.main_view)).addView(actionButton);
     }
 
     @Override
